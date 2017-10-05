fdbd74814027c2900225844cc515aed0ced1bbde
Julian Vollmer <>
2017-10-05 21:19:45 +0200

    add function

--- football.js
+++ football.js
@@ -1,5 +1,5 @@
 var League = require('./lib/league.js');
-var Team = require('./lib/Team.js');
+var Team = require('./lib/team.js');
 var Fixtures = require('./lib/fixtures.js');
 var Players = require('./lib/players.js');
 var LeagueTable = require('./lib/leagueTable.js');
@@ -40,7 +40,8 @@ function Football(options, callback) {
 	}
 
 	this.isInitialized = function () {
-		callback();
+		if(callback)
+			callback();
 	}
 
 	this.initLeague();
